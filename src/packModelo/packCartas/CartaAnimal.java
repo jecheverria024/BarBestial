5d8ed2e9ef6309d301144a0044c8f0def9faf7f3
dmarcos7 <davidmarcosnatividad@gmail.com>
2018-03-11 18:06:02 +0100

    aa

--- src/packModelo/packCartas/CartaAnimal.java
+++ src/packModelo/packCartas/CartaAnimal.java
@@ -2,4 +2,9 @@ package packModelo.packCartas;
 
 public class CartaAnimal extends CartaGeneral {
 
+	public CartaAnimal(String pImagen) {
+		super(pImagen);
+		// TODO Auto-generated constructor stub
+	}
+
 }
