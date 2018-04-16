9e2b474fc573385ebe976fd49a7c286f68850e47
jmartinez158 <julenmtz8m@gmail.com>
2018-04-16 11:46:25 +0200

    cambios

--- src/packModelo/Animalada/ALoro.java
+++ src/packModelo/Animalada/ALoro.java
@@ -2,6 +2,8 @@ package packModelo.Animalada;
 
 import java.util.Scanner;
 
+import packInterface.VentanaPedirPosicion;
+import packInterface.VentanaPrincipal;
 import packModelo.ColaEntrada;
 
 public class ALoro implements IAnimalada {
@@ -10,8 +12,9 @@ public class ALoro implements IAnimalada {
 	public void hacerAnimalada(int pFuerza, String pColor) {
 		int num = 0;
 		boolean correcto = false;
-		if (ColaEntrada.getColaEntrada().getLista().longitud() == 0) {
-
+		if (ColaEntrada.getColaEntrada().getLista().longitud() > 0) {
+			//VentanaPedirPosicion frame = new VentanaPedirPosicion(ColaEntrada.getColaEntrada().getLista().longitud());
+			//frame.setVisible(true);
 			while (!correcto) {
 				Scanner sc = new Scanner(System.in);
 				System.out.println("Inserta la posicion del animal que deseas eliminar");
